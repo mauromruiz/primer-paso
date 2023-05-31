249c0a1f57c398a2eb1c60dd8820f577411b8428
mauromruiz <35923304+mauromruiz@users.noreply.github.com>
2023-05-31 17:52:10 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+function showAlert() { alert('Hola!, esta es una alerta desde JavaScript!');}
+
+//let boton = document.getElementById('miBoton');
+
+//boton.addEventListener("click", function(){
+//    alert("Haz hecho clic en el boton!");
+//});
+
+//const miBoton = document.getElementById('miBoton');
+//function handleClick(){
+//    alert('Hola haz hecho clic');
+//}
+
+const form = document.getElementById('myForm');
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  validateForm();
+});
+
+function validateEmail(email) {
+    const regex =  /^[^\s@]+@[^\s@]+\.[^\s@]{2,7}$/
+    return regex.test(email)
+}
+
+function validateForm() {
+    const emailInput = document.getElementById('email');
+    const email = emailInput.value;
+    if (!validateEmail(email)) {
+      alert('Por favor ingrese un correo electrónico válido.');
+    } else {
+      alert('Correo electrónico enviado correctamente.');
+    }
+}
+
+document.querySelector("button.button-menu-toggle")
+.addEventListener("click", function(){
+  document.querySelector(".nav-links").
+    classList.toggle("nav-links-responsive")})
